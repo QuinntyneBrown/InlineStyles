34ed296d7a376f135def84256874d3b789404d85
QuinntyneBrown <quinntynebrown@gmail.com>
2021-05-05 23:14:36 -0400

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InlineComponent } from './inline/inline.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", component: InlineComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
